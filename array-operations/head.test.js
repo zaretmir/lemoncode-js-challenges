5eab46307aeb5cea464962a98bf216f332800918
zaretmir <nazaretmiranda@gmail.com>
2020-11-12 21:15:13 +0000

    :green_circle: Add test to ensure the input array is not mutated.

--- array-operations/head.test.js
+++ array-operations/head.test.js
@@ -1,6 +1,13 @@
 const head = require('./head')
 
 test('Extracts the first element of a given array', () => {
-    let anArray = ['head', 0, { anyProp: 'anyValue'}]
-    expect(head(anArray)).toEqual('head')
+    let anArray = ['head', 0, { anyProp: 'anyValue'}];
+    expect(head(anArray)).toEqual('head');
+})
+
+test('The input array is not mutated', () => {
+    let anArray = ['head', 0, '1'];
+    let anArrayCopy = [...anArray];
+    head(anArray);
+    expect(anArray).toEqual(anArrayCopy)
 })
