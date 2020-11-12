685e772117e0d686761ba8c285dbd10d41edcf1b
zaretmir <nazaretmiranda@gmail.com>
2020-11-12 22:14:59 +0000

    :green_circle: Add test to ensure the input array is not mutated.

--- array-operations/tail.test.js
+++ array-operations/tail.test.js
@@ -4,3 +4,10 @@ test('Outputs all elements except the first one of an array passed as argument',
     let anArray = [{ anyProp: 'anyValue'}, 'tail1', 'tail2'];
     expect(tail(anArray)).toEqual(['tail1', 'tail2']);
 })
+
+test('The input array is not mutated', () => {
+    let anArray = [{ anyProp: 'anyValue'}, 'tail1', 'tail2'];
+    let anArrayCopy = [...anArray];
+    tail(anArray);
+    expect(anArray).toEqual(anArrayCopy)
+})
