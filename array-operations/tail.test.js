62369a26c9f698b6ad7f731ef84364ce6126d794
zaretmir <nazaretmiranda@gmail.com>
2020-11-12 22:11:01 +0000

    :bug: Fix test that was checking a wrong requirement.

--- array-operations/tail.test.js
+++ array-operations/tail.test.js
@@ -1,6 +1,6 @@
 const tail = require('./tail')
 
-test('Outputs the last element of an array passed as argument', () => {
-    let anArray = [{ anyProp: 'anyValue'}, 0, 'tail'];
-    expect(tail(anArray)).toEqual('tail')
+test('Outputs all elements except the first one of an array passed as argument', () => {
+    let anArray = [{ anyProp: 'anyValue'}, 'tail1', 'tail2'];
+    expect(tail(anArray)).toEqual(['tail1', 'tail2']);
 })
