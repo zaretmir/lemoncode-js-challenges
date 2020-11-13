7dd2ca720791981092de674ba6be2dfdb849dd51
zaretmir <nazaretmiranda@gmail.com>
2020-11-13 20:24:41 +0000

    :bug: Fix test.

--- clone-merge/clone.test.js
+++ clone-merge/clone.test.js
@@ -3,5 +3,5 @@ const clone = require('./clone')
 test('Receives an object to output a new object with the same properties', () => {
     let anObject = { 'key1': 1, 'key2': 'two'};
     expect(clone(anObject)).toEqual(anObject);
-    expect(clone(anObject)).not.toStrictEqual(anObject);
+    expect(clone(anObject)).not.toBe(anObject);
 });
