7de2e14d166af0406566db9d863b26aad05783ae
zaretmir <nazaretmiranda@gmail.com>
2020-11-13 15:20:36 +0000

    :green_circle: Add passing test to ensure last does not mutate the given array.

--- array-operations/last.test.js
+++ array-operations/last.test.js
@@ -4,3 +4,10 @@ test('Outputs the last element of a given array', () => {
     let anArray = ['element1', 'element2', 'element3', 'element4'];
     expect(last(anArray)).toEqual('element4');
 })
+
+test('last does not mutate the array passed as argument', () => {
+    let anArray = ['element1', 'element2', 'element3', 'element4'];
+    let anArrayCopy = [...anArray];
+    last(anArray);
+    expect(anArray).toEqual(anArrayCopy);
+})
