a0febdac08f040afc55e5fe63449818ba0b1b70c
zaretmir <nazaretmiranda@gmail.com>
2020-11-13 19:47:06 +0000

    :pencil2: Fix typo in test.

--- concat/concat-several.test.js
+++ concat/concat-several.test.js
@@ -1,6 +1,6 @@
 const concatSeveral = require('./concat-several')
 
-test('Receives two or more arrays two return the concatenation of all', () => {
+test('Receives two or more arrays to return the concatenation of all', () => {
     let first = ['a1', 'a2', 'a3'];
     let second = ['b1', 'b2', 'b3'];
     let third = ['c1', 'c2', 'c3'];
