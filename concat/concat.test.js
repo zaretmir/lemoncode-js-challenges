0a55e05a39efac3a0449a2efef1c45a1bbfb391e
zaretmir <nazaretmiranda@gmail.com>
2020-11-13 15:32:47 +0000

    :green_circle: Add passing test to ensure 'concat' is immutable.

--- concat/concat.test.js
+++ concat/concat.test.js
@@ -6,3 +6,13 @@ test('Receives two arrays to output their concatenation', () => {
 
     expect(concat(first, second)).toEqual(['a1', 'a2', 'a3', 'a4', 'b1', 'b2', 'b3', 'b4'])
 })
+
+test('None of the input arrays are mutated', () => {
+    let first = ['a1', 'a2', 'a3', 'a4'];
+    let firstCopy = ['a1', 'a2', 'a3', 'a4'];
+    let second = ['b1', 'b2', 'b3', 'b4'];
+    let secondCopy = ['b1', 'b2', 'b3', 'b4'];
+    concat(first, second)
+    expect(first).toEqual(firstCopy);
+    expect(second).toEqual(secondCopy);
+})
