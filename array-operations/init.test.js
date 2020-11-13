9b23c5abb8a1ad78370a68658d6f366ca206ddcb
zaretmir <nazaretmiranda@gmail.com>
2020-11-13 15:05:47 +0000

    :green_circle: Add passing test to ensure the input array is not mutated.

--- array-operations/init.test.js
+++ array-operations/init.test.js
@@ -4,3 +4,10 @@ test('Outputs all elements of an array except the last one', () => {
     let anArray = ['element1', 'element2', 'element3', 'element4'];
     expect(init(anArray)).toEqual(['element1', 'element2', 'element3']);
 })
+
+test('Init does not mutate the array passed as argument', () => {
+    let anArray = ['element1', 'element2', 'element3', 'element4'];
+    let anArrayCopy = [...anArray];
+    init(anArray);
+    expect(anArray).toEqual(anArrayCopy);
+})
