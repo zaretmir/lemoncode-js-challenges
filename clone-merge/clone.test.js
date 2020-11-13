263c0799aba85c94598078356cdbeece89db7d55
zaretmir <nazaretmiranda@gmail.com>
2020-11-13 20:23:06 +0000

    :red_circle: Add failing test to ensure 'clone' output a new object with the same properties.

---
+++ clone-merge/clone.test.js
@@ -0,0 +1,7 @@
+const clone = require('./clone')
+
+test('Receives an object to output a new object with the same properties', () => {
+    let anObject = { 'key1': 1, 'key2': 'two'};
+    expect(clone(anObject)).toEqual(anObject);
+    expect(clone(anObject)).not.toStrictEqual(anObject);
+});
