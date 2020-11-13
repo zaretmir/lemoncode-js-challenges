faa24458b949d1b270cc1158eb9d439205aac141
zaretmir <nazaretmiranda@gmail.com>
2020-11-13 21:10:26 +0000

    :red_circle: Add failing test to ensure the values of the first object prevail in case of collision.

--- clone-merge/merge.test.js
+++ clone-merge/merge.test.js
@@ -5,3 +5,9 @@ test('Receives two objects to output an object with the properties of both', () 
     let anotherObject = { b1: 'B1', b2: 'B2'};
     expect(merge(anObject, anotherObject)).toEqual({ a1: 'A1', a2: 'A2',  b1: 'B1', b2: 'B2'});
 })
+
+test('If both input objects have a property with the same name, then the value of the first one prevails', () => {
+    let anObject = { repeatedKey: 'prevailed', a: 'A'};
+    let anotherObject = { repeatedKey: 'discarded', b: 'B'};
+    expect(merge(anObject, anotherObject)).toEqual({ repeatedKey: 'prevailed', a: 'A',  b: 'B'});
+})
