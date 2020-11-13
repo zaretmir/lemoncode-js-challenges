3a555bae8286db36a3e082fb24206500f33862fe
zaretmir <nazaretmiranda@gmail.com>
2020-11-13 15:46:55 +0000

    :red_circle: Add failing test to ensure 'concatSeveral' concatenates two or more arrays.

---
+++ concat/concat-several.test.js
@@ -0,0 +1,8 @@
+const concatSeveral = require('./concat-several')
+
+test('Receives two or more arrays two return the concatenation of all', () => {
+    let first = ['a1', 'a2', 'a3'];
+    let second = ['b1', 'b2', 'b3'];
+    let third = ['c1', 'c2', 'c3'];
+    expect(concatSeveral(first, second, third)).toEqual(['a1', 'a2', 'a3', 'b1', 'b2', 'b3', 'c1', 'c2', 'c3']);
+})
