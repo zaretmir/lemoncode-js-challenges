92112d12f0ea179d1350deb18d887e735b45ff10
zaretmir <nazaretmiranda@gmail.com>
2020-11-14 13:00:01 +0000

    :green_circle: :recycle: Add isWinningCombination, and call each time we play to pass test.

--- slot-machine/slot-machine.js
+++ slot-machine/slot-machine.js
@@ -5,6 +5,20 @@ class SlotMachine {
 
     play() {
         this.coins++;
+        if (this._isWinningCombination()) {
+            // tbi
+        } else {
+            // tbi
+        }
+    }
+
+    _isWinningCombination() {
+        return this._getRandomBoolean() && this._getRandomBoolean() && this._getRandomBoolean();
+    }
+
+    _getRandomBoolean() {
+        const probabilityOfTrue = 0.5;
+        return Math.random() < probabilityOfTrue;
     }
 }
 
