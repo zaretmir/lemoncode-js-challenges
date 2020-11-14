0d52314c6adeeb1cd32079ed063d8d18232ecbd9
zaretmir <nazaretmiranda@gmail.com>
2020-11-14 12:57:45 +0000

    :red_circle: :recycle: Add test to ensure we calculate a combination to determine game result, each time we play. Move tests inside 'describe'.

--- slot-machine/slot-machine.test.js
+++ slot-machine/slot-machine.test.js
@@ -1,9 +1,19 @@
 const SlotMachine = require('./slot-machine')
 
-it('Increments by one the number of coins, each time we play', () => {
-    const machine = new SlotMachine();
-    let initialCoins = machine.coins;
-    machine.play()
-    let coinsAfterOnePlay = machine.coins;
-    expect(coinsAfterOnePlay).toEqual(++initialCoins);
+describe('play', () => {
+    it('Increments by one the number of coins, each time we play', () => {
+        const machine = new SlotMachine();
+        let initialCoins = machine.coins;
+        machine.play()
+        let coinsAfterOnePlay = machine.coins;
+        expect(coinsAfterOnePlay).toEqual(++initialCoins);
+    });
+
+    it('Calculates a combination to determine if player won', () => {
+        let mockIsWinningCombination = jest.fn();
+        SlotMachine.prototype._isWinningCombination = mockIsWinningCombination
+        const machine = new SlotMachine();
+        machine.play()
+        expect(mockIsWinningCombination).toHaveBeenCalledTimes(1);
+    })
 })
